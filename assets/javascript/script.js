54a58a2bab6a73bd9809ed12277f02aebe8c92f8
Ajschlander <Aschlander21@gmail.com>
2019-11-15 13:18:06 -0600

    Added id's to question, choice, and answer fields.

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,38 +1,48 @@
-let questions = [
-    {
-        title: "What does JS stand for?",
-        choices: ["JavaSpoon", "JavaScript", "JustStyling", "Just sayin"],
-        answer: "JavaScript"
-    },
-    {
-        title: "What does JS stand for?",
-        choices: ["JavaSpoon", "JavaScript", "JustStyling", "Just sayin"],
-        answer: "JavaScript"
-    },
-    {
-        title: "What does JS stand for?",
-        choices: ["JavaSpoon", "JavaScript", "JustStyling", "Just sayin"],
-        answer: "JavaScript"
-    },
-    {
-        title: "What does JS stand for?",
-        choices: ["JavaSpoon", "JavaScript", "JustStyling", "Just sayin"],
-        answer: "JavaScript"
-    },
-    {
-        title: "What does JS stand for?",
-        choices: ["JavaSpoon", "JavaScript", "JustStyling", "Just sayin"],
-        answer: "JavaScript"
-    }
-];
-
 function startQuiz() {
-  
+
+  // Create an array of questions, choices, and answers
+  let questions = [
+      {
+          title: "What does JS stand for?",
+          choices: ["JavaSpoon", "JavaScript", "JustStyling", "Just sayin"],
+          answer: "JavaScript"
+      },
+      {
+          title: "What does JS stand for?",
+          choices: ["JavaSpoon", "JavaScript", "JustStyling", "Just sayin"],
+          answer: "JavaScript"
+      },
+      {
+          title: "What does JS stand for?",
+          choices: ["JavaSpoon", "JavaScript", "JustStyling", "Just sayin"],
+          answer: "JavaScript"
+      },
+      {
+          title: "What does JS stand for?",
+          choices: ["JavaSpoon", "JavaScript", "JustStyling", "Just sayin"],
+          answer: "JavaScript"
+      },
+      {
+          title: "What does JS stand for?",
+          choices: ["JavaSpoon", "JavaScript", "JustStyling", "Just sayin"],
+          answer: "JavaScript"
+      }
+  ];
+  // Select the startbutton
+  const startButton = document.getElementById('start-button');
+  // Select the
+  const questionField = document.getElementById('question');
+  const choicesField = document.getElementById('choices');
+
+  // Add event listener to start button
+  startButton.addEventListener("click", function(){
+    this.classList.toggle("hidden");
+    questionField.classList.toggle("hidden");
+
+
+
+  });
+
 }
 
-let startButton = document.getElementById('start-button');
-
-startButton.addEventListener("click", function(){
-  this.style.visibility = "hidden";
-  startQuiz();
-});
+startQuiz();
