5d3490e29664defda93f81fd83d82daf8ffde2f5
Ajschlander <Aschlander21@gmail.com>
2019-11-14 19:28:33 -0600

    Added start quiz button and more styling

---
+++ assets/javascript/script.js
@@ -0,0 +1,27 @@
+let questions = [
+    {
+        title: "What does JS stand for?",
+        choices: ["JavaSpoon", "JavaScript", "JustStyling", "Just sayin"],
+        answer: "JavaScript"
+    },
+    {
+        title: "What does JS stand for?",
+        choices: ["JavaSpoon", "JavaScript", "JustStyling", "Just sayin"],
+        answer: "JavaScript"
+    },
+    {
+        title: "What does JS stand for?",
+        choices: ["JavaSpoon", "JavaScript", "JustStyling", "Just sayin"],
+        answer: "JavaScript"
+    },
+    {
+        title: "What does JS stand for?",
+        choices: ["JavaSpoon", "JavaScript", "JustStyling", "Just sayin"],
+        answer: "JavaScript"
+    },
+    {
+        title: "What does JS stand for?",
+        choices: ["JavaSpoon", "JavaScript", "JustStyling", "Just sayin"],
+        answer: "JavaScript"
+    }
+]
