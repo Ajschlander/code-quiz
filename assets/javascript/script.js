5393a8dc851c82bfc3dc3a5fa3b8f12c9013516f
Ajschlander <Aschlander21@gmail.com>
2019-11-18 10:39:37 -0600

    Add event listener to the choices displayed. Also made them appear after start quiz is clicked

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -28,20 +28,33 @@ function startQuiz() {
           answer: "JavaScript"
       }
   ];
+
   // Select the startbutton
   const startButton = document.getElementById('start-button');
-  // Select the
+  // Select the fields that will be updating
   const questionField = document.getElementById('question');
   const choicesField = document.getElementById('choices');
+  const choiceOptions = document.getElementsByClassName('choice');
 
   // Add event listener to start button
   startButton.addEventListener("click", function(){
     this.classList.toggle("hidden");
     questionField.classList.toggle("hidden");
-
-
-
+    choicesField.classList.toggle("hidden");
   });
+
+  // Add an event listener to the choices
+  for (var i = 0; i < choiceOptions.length; i++) {
+    const option = choiceOptions[i];
+    option.addEventListener("click", function(){
+      
+    });
+  }
+
+  // loop through the array of objects and populate the html with them
+  for (var i = 0; i < questions.length; i++) {
+    questionField.innerHTML = questions[i].title;
+  }
 
 }
 
