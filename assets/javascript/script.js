77a4338e0ded1dc88b2cb4221bdad85499ff215d
Ajschlander <Aschlander21@gmail.com>
2019-11-18 12:02:03 -0600

    Created a function to loop through array and a countdown function that are both called when start game is clicked

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -4,7 +4,12 @@ function startQuiz() {
   let questions = [
       {
           title: "What does JS stand for?",
-          choices: ["JavaSpoon", "JavaScript", "JustStyling", "Just sayin"],
+          choices: ["JavaSpoon", "XXXXXJavaScript", "JustStyling", "Just sayin"],
+          answer: "JavaScript"
+      },
+      {
+          title: "What does JS stand for?",
+          choices: ["JavaSpoon", "JavaScriptXXXXXX", "JustStyling", "Just sayin"],
           answer: "JavaScript"
       },
       {
@@ -19,12 +24,7 @@ function startQuiz() {
       },
       {
           title: "What does JS stand for?",
-          choices: ["JavaSpoon", "JavaScript", "JustStyling", "Just sayin"],
-          answer: "JavaScript"
-      },
-      {
-          title: "What does JS stand for?",
-          choices: ["JavaSpoon", "JavaScript", "JustStyling", "Just sayin"],
+          choices: ["JavaSpoon", "JavaScripXXXt", "JustStyling", "Just sayin"],
           answer: "JavaScript"
       }
   ];
@@ -33,33 +33,54 @@ function startQuiz() {
   const startButton = document.getElementById('start-button');
   // Select the fields that will be updating
   const questionField = document.getElementById('question');
+  const line = document.getElementById('hr-line')
   const choicesField = document.getElementById('choices');
   const choiceOptions = document.getElementsByClassName('choiceArea');
   const choiceText = document.getElementsByClassName('option');
+  let timer = document.getElementById('countdown');
+
+  function loopObjectArray(){
+    // Loop through the array of objects
+    for (var i = 0; i < questions.length; i++) {
+      questionField.innerHTML = questions[i].title;
+      console.log(questions[i].title);
+      // Loop through the array of choices inside the object
+      for (var x = 0; x < questions[i].choices.length; x++) {
+        const choice = questions[i].choices[x];
+        choiceText[x].innerHTML = choice;
+        console.log(choice);
+      }
+    }
+  }
+
+  function countdown(){
+    let sec = 75;
+    countdown = setInterval(function(){
+        timer.innerHTML = sec + " seconds";
+        sec--;
+        if (sec < 0) {
+            clearInterval(countdown);
+        }
+    }, 1000);
+    console.log("this is the timer function");
+}
 
   // Add event listener to start button
   startButton.addEventListener("click", function(){
     this.classList.toggle("hidden");
     questionField.classList.toggle("hidden");
     choicesField.classList.toggle("hidden");
+    line.classList.toggle("hidden");
+    countdown();
+    loopObjectArray();
   });
-
-  // Loop through the array of objects
-  for (var i = 0; i < questions.length; i++) {
-    questionField.innerHTML = questions[i].title;
-    // Loop through the array of choices inside the object
-    for (var i = 0; i < questions[i].choices.length; i++) {
-      const choice = questions[i].choices[i];
-      choiceText[i].innerHTML = [i+1] + ". " + choice;
-    }
-  }
 
   // Add an event listener to the choices
   for (var i = 0; i < choiceOptions.length; i++) {
     const option = choiceOptions[i];
     option.addEventListener("click", function(){
       // check to see if the option clicked is the same as the answer
-
+      console.log(option.innerHTML);
     });
   }
 }
